772ed0444583f10c77e7eb9e25609c8905c7cf0b
Frédéric LeBlanc-Graveline <frederic.leblanc.graveline@gmail.com>
2020-02-04 11:31:05 -0500

    Validate the bearer's uid with the provided uid to ensure a match for additionnal security
    Débatable, probablement très inutile

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,13 +3,17 @@ const { authenticate } = require('../utils.js');
 const Query = {
   users: (root, args, context) => context.prisma.users(),
   login: (root, args, context) => authenticate(context)
-    .then(() => context.prisma.$exists.user({userId: args.userId}) 
-    )
+    .then((validatedUser) => {
+      if (args.userId && validatedUser.uid) {
+        return context.prisma.$exists.user({ userId: args.userId });
+      }
+      console.log('Provided UID doesn\'t match with the bearer token');
+      throw new Error('auth/invalid_uid');
+    })
     .catch((error) => {
       console.error(error);
       return error;
     }),
-    
 };
 
 module.exports = { Query };
