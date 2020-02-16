cc4631c8c2745c7b36ccd0c266772b29b8ce53f9
Philippe Courteau <coup2307@usherbrooke.ca>
2020-02-16 15:31:01 -0500

    fixed mutation with input

--- src/resolvers/Mutation/user.mutation.js
+++ src/resolvers/Mutation/user.mutation.js
@@ -3,11 +3,10 @@ const { authenticate } = require('../../utils.js');
 const userMutation = {
   signUp: (root, args, context) => authenticate(context)
     .then(() => context.prisma.createUser({
-      userId: args.userId,
-      name: args.name,
-      lastName: args.lastName,
-      email: args.email,
-      avatar: args.avatar,
+      userId: args.input.userId,
+      name: args.input.name,
+      lastName: args.input.lastName,
+      email: args.input.email,
     }))
     .catch((error) => error),
 };
