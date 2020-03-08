c086252a0c965bb914d28ccce886a2d4569ae8c9
voys3101 <samuel.voyer@usherbrooke.ca>
2020-03-08 15:11:16 -0400

    change Query files structure

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,14 +3,19 @@ const { groupMutation } = require('./Mutation/group');
 const { addressMutation } = require('./Mutation/address');
 const { invitationMutation } = require('./Mutation/invitation');
 
-const { Query } = require('./Query');
+const { Query } = require('./Query/Query');
+const { transactionQuery } = require('./Query/transaction');
+
 const { User } = require('./User');
 const { Group } = require('./Group');
 const { Invitation } = require('./Invitation');
 const { UserGroup } = require('./UserGroup');
 
 module.exports = {
-  Query,
+  Query: {
+    ...Query,
+    ...transactionQuery,
+  },
   Mutation: {
     ...userMutation,
     ...groupMutation,
