bdce1c160d97ad9d7c897be568f77be6a8e18c0f
Khalil Mansour <khalilman_96@live.ca>
2020-03-26 15:45:23 -0400

    UserGroup subscription fixed

--- src/resolvers/Subscription/userGroup.js
+++ src/resolvers/Subscription/userGroup.js
@@ -1,17 +1,17 @@
 const { authenticate, userBelongsToGroup  } = require('../../utils.js');
 
-const groupSubscription = {
-  group: {
+const userGroupSubscription = {
+  userGroup: {
     subscribe: async (parent, args, context) => {
       try {
         const res = await authenticate(context);
 
         // make sure that the connected user is allowed to subscribe to the removals of the specified group
         if (!(await userBelongsToGroup(context, res.uid, args.input.groupId))) {
-          throw new Error('The connected user is not allowed to subscribe to the transactions of the specified group.');
+          throw new Error('The connected user is not allowed to subscribe to the changes of the specified group.');
         }
 
-        return context.prisma.$subscribe.group({
+        return context.prisma.$subscribe.userGroup({
           mutation_in: ['CREATED', 'UPDATED', 'DELETED'],
           node: {
             id: args.input.groupId,
@@ -22,9 +22,8 @@ const groupSubscription = {
         throw new Error(error.message);
       }            
     },
-
     resolve: (payload) => payload,
   },
 };
 
-module.exports = { groupSubscription };
+module.exports = { userGroupSubscription };
